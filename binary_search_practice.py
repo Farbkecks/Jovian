f3eff2c0ad4719ee6ac603717d4873ee587dd01a
Farbkecks <fabian.alich@gmx.de>
2022-08-07 22:08:27 +0200

    repeating nummbers

--- binary_search_practice.py
+++ binary_search_practice.py
@@ -58,34 +58,52 @@ Optimize:
 #         if mid_nummber > last_numbber:
 #             lo = mid + 1
 
-# Kopiert von Jovian
-def binary_search(lo, hi, condition):
-    while lo <= hi:
-        mid = (lo + hi) // 2
-        result = condition(mid)
-        if result == "found":
-            return mid
-        elif result == "left":
-            hi = mid - 1
-        else:
-            lo = mid + 1
-    return -1
+# # Kopiert von Jovian
+# def binary_search(lo, hi, condition):
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         result = condition(mid)
+#         if result == "found":
+#             return mid
+#         elif result == "left":
+#             hi = mid - 1
+#         else:
+#             lo = mid + 1
+#     return -1
+
+
+# def count_rotations(nums: list) -> int:
+#     if len(nums) == 1:
+#         return 0
+
+#     def condition(mid):
+#         last_numbber = nums[-1]
+#         if nums[mid - 1] > nums[mid]:
+#             return "found"
+#         if nums[mid] < last_numbber:
+#             return "left"
+#         if nums[mid] > last_numbber:
+#             return "right"
+
+#     return binary_search(0, len(nums) - 1, condition)
 
 
 def count_rotations(nums: list) -> int:
+    if len(nums) == 0:
+        return -1
     if len(nums) == 1:
         return 0
-
-    def condition(mid):
-        last_numbber = nums[-1]
-        if nums[mid - 1] > nums[mid]:
-            return "found"
-        if nums[mid] < last_numbber:
-            return "left"
-        if nums[mid] > last_numbber:
-            return "right"
-
-    return binary_search(0, len(nums) - 1, condition)
+    lo, hi = 0, len(nums)
+    last_numbber = nums[-1]
+    while True:
+        mid = (lo + hi) // 2
+        mid_nummber = nums[mid]
+        if mid_nummber < nums[mid - 1]:
+            return mid
+        if mid_nummber < last_numbber:
+            hi = mid - 1
+        if mid_nummber > last_numbber:
+            lo = mid + 1
 
 
 tests = []
@@ -95,7 +113,11 @@ tests.append({"input": {"nums": [0, 2, 3, 4]}, "output": 0})
 tests.append({"input": {"nums": [20, 24, 25, 30, 1]}, "output": 4})
 tests.append({"input": {"nums": []}, "output": -1})
 tests.append({"input": {"nums": [5, 6, 7, 2, 3, 4]}, "output": 3})
+tests.append({"input": {"nums": [5, 6, 7, -1, 2, 3, 4]}, "output": 3})
 tests.append({"input": {"nums": [1]}, "output": 0})
+tests.append(
+    {"input": {"nums": [5, 6, 6, 9, 9, 9, 0, 0, 2, 3, 3, 3, 3, 4, 4]}, "output": 6}
+)
 
 
 test_count = 1
