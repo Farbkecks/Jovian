81cc2f53dd0e5cf8989f8b6b98d3e12677750022
Farbkecks <fabian.alich@gmx.de>
2022-08-09 13:52:23 +0200

    delete douple

--- binary_search_practice.py
+++ binary_search_practice.py
@@ -41,23 +41,6 @@ Optimize:
 #     return 0
 
 
-# def count_rotations(nums: list) -> int:
-#     if len(nums) == 0:
-#         return -1
-#     if len(nums) == 1:
-#         return 0
-#     lo, hi = 0, len(nums)
-#     last_numbber = nums[-1]
-#     while True:
-#         mid = (lo + hi) // 2
-#         mid_nummber = nums[mid]
-#         if mid_nummber < nums[mid - 1]:
-#             return mid
-#         if mid_nummber < last_numbber:
-#             hi = mid - 1
-#         if mid_nummber > last_numbber:
-#             lo = mid + 1
-
 # # Kopiert von Jovian
 # def binary_search(lo, hi, condition):
 #     while lo <= hi:
@@ -93,7 +76,7 @@ def count_rotations(nums: list) -> int:
         return -1
     if len(nums) == 1:
         return 0
-    lo, hi = 0, len(nums) - 1
+    lo, hi = 0, len(nums)
     last_numbber = nums[-1]
     while True:
         mid = (lo + hi) // 2
