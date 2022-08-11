32f5cc36257936a2d0ad15f9e6e3c660f0f48731
Farbkecks <fabian.alich@gmx.de>
2022-08-11 18:34:33 +0200

    print(traversal_pre_order

--- binary_tree_#1.py
+++ binary_tree_#1.py
@@ -18,6 +18,25 @@ def create_node_from_tuple(data: tuple) -> TreeNode:
     return node
 
 
+def display_keys(node, space="\t", level=0):
+    # print(node.key if node else None, level)
+
+    # If the node is empty
+    if node is None:
+        print(space * level + "∅")
+        return
+
+    # If the node is a leaf
+    if node.left is None and node.right is None:
+        print(space * level + str(node.key))
+        return
+
+    # If the node has children
+    display_keys(node.right, space, level + 1)
+    print(space * level + str(node.key))
+    display_keys(node.left, space, level + 1)
+
+
 def create_tuple_from_node(node: TreeNode) -> tuple:
     def check_for_leef(node: TreeNode):
         if node.left == None and node.right == None:
@@ -34,10 +53,18 @@ def create_tuple_from_node(node: TreeNode) -> tuple:
     return (get_value_from_note(node.left), node.key, get_value_from_note(node.right))
 
 
+def traversal_pre_order(node, data=[]):
+    if node.key != 0:
+        data.append(node.key)
+    if node.left != None:
+        data = traversal_pre_order(node.left, data)
+    if node.right != None:
+        data = traversal_pre_order(node.right, data)
+    return data
+
+
 if __name__ == "__main__":
     data = ((1, 3, 0), 2, ((0, 3, 4), 5, (6, 7, 8)))
     tree = create_node_from_tuple(data)
-    # print(tree.left.key, tree.key, tree.right.key)
-    # print(tree.right.left.key, tree.right.key, tree.right.right.key)
-    reversed_data = create_tuple_from_node(tree)
-    print(reversed_data == data)
+    display_keys(tree)
+    # print("richtig: 231534768")
