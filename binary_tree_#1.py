87dc131a3bc124e869585603d9aa52d31c116981
Farbkecks <fabian.alich@gmx.de>
2022-08-11 16:08:50 +0200

    get tuple from node

--- binary_tree_#1.py
+++ binary_tree_#1.py
@@ -5,12 +5,12 @@ class TreeNode:
         self.left: TreeNode = None
 
 
-def create_node_from_tuble(data: tuple) -> TreeNode:
+def create_node_from_tuple(data: tuple) -> TreeNode:
     node = TreeNode(data[1])
     if type(data[0]) == tuple:
-        node.left = create_node_from_tuble(data[0])
+        node.left = create_node_from_tuple(data[0])
     if type(data[2]) == tuple:
-        node.right = create_node_from_tuble(data[2])
+        node.right = create_node_from_tuple(data[2])
     if type(data[0]) == int:
         node.left = TreeNode(data[0])
     if type(data[2]) == int:
@@ -18,7 +18,26 @@ def create_node_from_tuble(data: tuple) -> TreeNode:
     return node
 
 
+def create_tuple_from_node(node: TreeNode) -> tuple:
+    def check_for_leef(node: TreeNode):
+        if node.left == None and node.right == None:
+            return True
+        else:
+            return False
+
+    def get_value_from_note(node: TreeNode):
+        if check_for_leef(node):
+            return node.key
+        else:
+            return create_tuple_from_node(node)
+
+    return (get_value_from_note(node.left), node.key, get_value_from_note(node.right))
+
+
 if __name__ == "__main__":
-    tree = create_node_from_tuble((1, 2, (3, 4, 5)))
-    print(tree.left.key, tree.key, tree.right.key)
-    print(tree.right.left.key, tree.right.key, tree.right.right.key)
+    data = ((1, 3, 0), 2, ((0, 3, 4), 5, (6, 7, 8)))
+    tree = create_node_from_tuple(data)
+    # print(tree.left.key, tree.key, tree.right.key)
+    # print(tree.right.left.key, tree.right.key, tree.right.right.key)
+    reversed_data = create_tuple_from_node(tree)
+    print(reversed_data == data)
