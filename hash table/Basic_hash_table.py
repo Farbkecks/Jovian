12b69282286aa2097a22b99ca8e3efb4f89eacb9
Farbkecks <fabian.alich@gmx.de>
2022-08-19 12:21:12 +0200

    fix collision

--- hash table/Basic_hash_table.py
+++ hash table/Basic_hash_table.py
@@ -6,15 +6,28 @@ class Basic_hash_table:
         resulte = 0
         for char in key:
             resulte += ord(char)
-        return resulte % len(self.list)
+        index = resulte % len(self.list)
+        return self.get_valid_index(key, index)
+
+    def get_valid_index(self, key, index):
+        while True:
+            item = self.list[index]
+            if item == None:
+                return index
+            if item[0] == key:
+                return index
+            index += 1
+            if index == len(self.list) - 1:
+                index = 0
 
     def insert(self, key, value):
         """Insert a new key-value pair"""
         self.list[self.__get_index(key)] = (key, value)  # type: ignore (VSCode gibt einen Error den es nicht gibt)
 
-    def find(self, key):
+    def find_print(self, key):
         """Find the value associated with a key"""
-        return self.list[self.__get_index(key)]
+        # return self.list[self.__get_index(key)]
+        print(self.list[self.__get_index(key)])
 
     def update(self, key, value):
         """Change the value associated with a key"""
@@ -28,8 +41,7 @@ class Basic_hash_table:
 if __name__ == "__main__":
     table = Basic_hash_table()
     table.insert("Fabian", "Ich bin der Owner")
-    print(table.find("Fabian"))
-    table.update("Fabian", "hallo")
-    print(table.find("Fabian"))
-    table.insert("sdfjkdlf", "sjdfklsdf")
-    print(table.list_all())
+    table.insert("listen", "1")
+    table.insert("silent", "2")
+    table.find_print("listen")
+    table.find_print("silent")
